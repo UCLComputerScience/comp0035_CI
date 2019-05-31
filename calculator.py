e3a79fe91dd2e8ea270d247b68e42111c09bf15d
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-05-31 11:38:03 +0100

    version to test the CircleCI integration

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
+"""
+Calculator containing basic math operations.
+"""
+
 def add(first_term, second_term):
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
     return first_term - second_term
-
